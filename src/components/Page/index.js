8340d1afb3cbb8576bf780193a53f87d200cbdd5
Prakhar Mehrotra <prakhar1@gmail.com>
2021-12-01 16:59:37 +0100

    setup template

---
+++ src/components/Page/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import styles from "./Page.module.sass";
+import { clearAllBodyScrollLocks } from "body-scroll-lock";
+import Ribbon from "../Ribbon";
+//import Footer from "../Footer";
+
+const Page = ({ 
+  seperatorHeader,
+  children,
+  footerHide,
+  wide,
+  notAuthorized
+}) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    clearAllBodyScrollLocks();
+  }, [pathname]);
+
+  return (
+    <>
+      <div className={styles.page}>
+        <Ribbon 
+          seperatorHeader={seperatorHeader}
+          wide={wide}
+          notAuthorized={notAuthorized}
+        />
+        <div className={styles.inner}>{children}</div>
+        {/* {!footerHide && <Footer />} */}
+      </div>
+    </>
+    );
+  };
+
+  export default Page;
---
+++ src/components/Ribbon/index.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import styles from "./Ribbon.module.sass";
+
+
+const Ribbon = ({}) => {
+
+  return (
+    <>
+      <div className={styles.ribbon}>
+        <h5 className={styles.ribbonText}>Shop Now. Pay with DLL Finance. Learn More</h5>
+      </div>
+    </>
+  );
+};
+export default Ribbon;
