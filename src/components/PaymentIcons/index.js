addda0a4b050c47bdfa0660ef4d2c7d6d6e18dac
Prakhar Mehrotra <prakhar1@gmail.com>
2021-12-03 16:20:59 +0100

    styled payment icons

--- src/components/PaymentIcons/index.js
+++ src/components/PaymentIcons/index.js
@@ -4,8 +4,8 @@ import styles from "./PaymentIcons.module.sass";
 const PaymentIcons = () => {
   return (
     <>
-      <div className={styles.paymentIcons}>
-        <img src="/images/paymentIcons.png" alt="Payment Options" />
+      <div className={styles.paymentIconsContainer}>
+        <img src="/images/paymentIcons.png" alt="Payment Options" className={styles.paymentIcons}/>
       </div>
     </>
   );
