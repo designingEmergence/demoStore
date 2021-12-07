0c3d5cb82ac0ec5f31c3f02574f2f3a2028af6ca
Prakhar Mehrotra <prakhar1@gmail.com>
2021-12-07 18:18:42 +0100

    Added content to product info page

--- src/components/AddToCart/index.js
+++ src/components/AddToCart/index.js
@@ -1,10 +1,43 @@
 import React, {useState} from "react";
 import styles from "./AddToCart.module.sass";
+import Dropdown from "../Dropdown";
+import cn from "classnames";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-const AddToCart = () => {
+function calculatePrice(price) {
+  return price/4+ price*0.2;
+}
+
+
+let financingOption = "Own It";
+let financingOptions = ["Own It", "Use It"]
+
+function selectFinancingOption(value) {
+  console.log('selected', value);
+  financingOption = value;
+}
+
+
+
+const AddToCart = ({price}) => {
   return (
     <>
-      <p>Add To Cart</p>
+      <div className={styles.addToCartContainer}>
+        <p className={styles.price}>{price}</p>
+        <div className={styles.dllPayment}>
+          <img src="/images/icons/paypal.svg" alt="dll" />
+          <div className={styles.dllPaymentTextColumn}>
+            <p className={styles.dllPaymentText}>4 interest free payments of {calculatePrice(price)}</p>
+            <p className={styles.dllPaymentText, styles.learnMore}>Learn More</p>
+          </div>
+          <Dropdown  
+            className={styles.dropdown} 
+            value={financingOption} 
+            setValue={selectFinancingOption}
+            options={financingOptions}/>
+        </div>
+        <button className={cn("button", styles.addToCartButton)}>Add to Cart <AddShoppingCartIcon /></button>
+      </div>
     </>
   );
 };
--- src/components/ProductInfo/index.js
+++ src/components/ProductInfo/index.js
@@ -1,10 +1,30 @@
 import React, {useState} from "react";
 import styles from "./ProductInfo.module.sass";
+import ProductGallery from "../ProductGallery";
+import AddToCart from "../AddToCart";
+import StarIcon from '@mui/icons-material/Star';
 
-const ProductInfo = () => {
+const ProductInfo = ({product}) => {
   return (
     <>
-      <p>ProductInfo</p>
+      {/* <p>ProductInfo</p> */}
+      <div className={styles.productInfoContainer}>
+        <div className={styles.column1}>
+          <p className={styles.productName}>{product.name}</p>
+          <div className={styles.productRating}>
+            <StarIcon className={styles.starIcon} />
+            <p className={styles.productRatingText}>{product.rating}</p>
+            <p className={styles.productRatingReviews}>({product.reviews} reviews)</p>
+          </div>
+          <ProductGallery images={product.images} />
+        </div>
+        <div className={styles.column2}>
+          <AddToCart price={product.price} />
+          <p className={styles.overview}>Overview</p>
+          <hr className={styles.hr} />
+          <p className={styles.description}>{product.description}</p>
+        </div>
+      </div>
     </>
   );
 };
