e8d273753e36ad12b080ad2ed21fcde45af263e5
Prakhar Mehrotra <prakhar1@gmail.com>
2021-12-22 12:02:44 +0100

    Created thank you page

--- src/pages/ThankYou/index.js
+++ src/pages/ThankYou/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './ThankYou.module.sass';
+import cn from 'classnames';
+import { useNavigate } from "react-router";
 
 const ThankYou = () => {
+  let navigate = useNavigate();
+
   return (
     <>
-      <p>Thank you</p> 
+      <div className={styles.thankYouContainer}>
+        <div className={styles.column1}>
+          <img className={styles.thankYouImage} src="/images/thankyou.png" alt="thankyou" />
+        </div>
+        <div className={styles.column2}>
+          <p className={styles.title}>Thank you for your purchase!</p>
+          <p className={styles.subtext}>Order Number: 239074238</p>
+          <p className={styles.subtext}>Your order has been confirmed and you will receive an order confirmation email shortly.</p>
+          <button onClick={()=> navigate('/')} className={cn('button', styles.continueButton)}>Continue Shopping</button>
+        </div>
+      </div>
     </>
   );
 };
 
-export default ThankYou;
+export default ThankYou; 
