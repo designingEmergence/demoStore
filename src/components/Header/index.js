4a605f386011f2685eac99cb58c222c550bd67ce
Mehrotra, Prakhar <prakhar.mehrotra@accenture.com>
2022-01-07 16:23:20 +0100

    Added item quantity to cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,38 @@
-import React, {useContext, useState, useRef, useEffect} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./Header.module.sass";
+import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import BagOverview from '../BagOverview';
-
+import { useCart } from "react-use-cart";
 import { Link } from "react-router-dom";
+
 
 const Header = ({}) => {
   const ref = useRef();
   let [showBag, setShowBag] = useState(false);
 
-
-
   useOnClickOutside(ref, () => setShowBag(false));
   const { pathname } = useLocation();
+  const { totalItems } = useCart();
 
   useEffect(() => {
     setShowBag(false);
   }, [pathname])
 
   return (
+    
     <>
       <div className={styles.header}>
         <Link to="/"><p className={styles.logoBlack}>Demostore</p></Link>
         <button onClick={() => {
           setShowBag(!showBag);
         }
-         } > <ShoppingCartIcon className={styles.cart}/></button>
+         } > 
+          <Badge  badgeContent={totalItems} color="primary">
+            <ShoppingCartIcon className={styles.cart}/>
+          </Badge>
+        </button>
         {showBag && <div className={styles.bagOverview} ><BagOverview showPaymentOptions={true}/></div>}
       </div>
     </>
