bf577dcfb0e750b8503d76ca33ef8d44be48240b
Mehrotra, Prakhar <prakhar.mehrotra@accenture.com>
2022-01-11 15:20:12 +0100

    read extras from config file

--- src/data/copyApple.js
+++ src/data/copyApple.js
@@ -4,7 +4,22 @@ const copy = {
     subtitle: 'Macbook Pro',
     price: '$1,999',
   },
+  assetDescriptor: "device",
   productGridName: 'Products',
+  extras: {
+    option1: {
+      text: 'Add Apple Care',
+      price: '$80/month',
+    },
+    option2: {
+      text: 'Add maintenance',
+      price: '$40/month',
+    },
+    option3: {
+      text: 'Add IoT Subscription',
+      price: '$25/month',
+    }
+  },
   shipping: {
     option1: {
       type: 'Deliver with UPS (free)',
