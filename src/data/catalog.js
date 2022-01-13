4158117f429e95c7fcd8fe186b3f98e0b9680ad4
Mehrotra, Prakhar <prakhar.mehrotra@accenture.com>
2022-01-13 09:33:38 +0100

    Revert back to apple store

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -1,11 +1,11 @@
 const productCatalog = [
   {
     "id": "1",
-    "name": "iTero Element Plus Series",
-    "description": "With the iTero Element Plus Series you have cutting-edge digital scanning performance that delivers a seamless experience. iTero Element Plus imaging systems are built upon the success of the iTero Element portfolio, which features innovative technology like Near-Infrared Technology (NIRI) to aid in caries detection above the gingiva1. Take your practice to the next level with enhanced productivity and patient experiences.",
-    "price": 19999,
+    "name": "MacBook Pro - M1 Pro",
+    "description": "The Apple MacBook Pro is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 1999,
     "image": "images/products/1/1.jpg",
-    "images": ["images/products/1/1.jpg", "images/products/1/2.jpg"],
+    "images": ["images/products/1/1.jpg", "images/products/1/2.jpg", "images/products/1/3.jpg", "images/products/1/4.jpg"],
     "available": true,
     "exclusive": true,
     "rating": 4.8,
@@ -13,11 +13,11 @@ const productCatalog = [
   },
   {
     "id": "2",
-    "name": "iTero Element 5D",
-    "description": "The iTero Element 5D is a revolutionary imaging system that delivers a seamless experience. iTero Element 5D imaging systems are built upon the success of the iTero Element portfolio, which features innovative technology like Near-Infrared Technology (NIRI) to aid in caries detection above the gingiva. Take your practice to the next level with enhanced productivity and patient experiences.",
-    "price": 23999,
+    "name": "iPad Pro",
+    "description": "The Apple iPad Pro is a premium 9.7-inch iPad that features a stunning Retina display. It is powered by the latest A10 Fusion chip and is packed with features such as wireless charging, wireless keyboard, and a built-in stylus. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 1399,
     "image": "images/products/2/1.jpg",
-    "images": ["images/products/2/1.jpg", "images/products/2/2.jpg"],
+    "images": ["images/products/2/1.jpg", "images/products/2/2.jpg", "images/products/2/3.jpg", "images/products/2/4.jpg"],
     "available": true,
     "exclusive": false,
     "rating": 4.7,
@@ -25,16 +25,64 @@ const productCatalog = [
   },
   {
     "id": "3",
-    "name": "iTero Element 2",
-    "description": "With the iTero Element 2 scanner in your practice, you've got a powerful system built to streamline your workflow, enhance your precision, and better connect with your patients. Features like its expanded 21.5” touch widescreen and high-definition color imaging make for more compelling chairside consultations. The iTero Element 2 scanner’s faster scan processing* and lab compatibility make it easier to grow your practice by accepting more case volume.",
-    "price": 39900,
+    "name": "Mac Pro",
+    "description": "The Apple Mac Pro is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 9888,
     "image": "images/products/3/1.jpg",
-    "images": ["images/products/3/1.jpg", "images/products/3/2.jpg"],
+    "images": ["images/products/3/1.jpg", "images/products/3/2.jpg", "images/products/3/3.jpg", "images/products/3/4.jpg"],
     "available": true,
     "exclusive": true,
     "rating": 4.4,
     "reviews": 5966
-  }
+  },
+  {
+    "id": "4",
+    "name": "Apple Pro Display",
+    "description": "The Apple Pro Display is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 4999,
+    "image": "images/products/4/1.jpg",
+    "images": ["images/products/4/1.jpg", "images/products/4/2.jpg", "images/products/4/3.jpg", "images/products/4/4.jpg"],
+    "available": true,
+    "exclusive": false,
+    "rating": 4.8,
+    "reviews": 1233
+  },
+  {
+    "id": "5",
+    "name": "Radeon Pro W6800X MPX Module",
+    "description": "The Apple Mac Pro is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 3200,
+    "image": "images/products/5/1.jpg",
+    "images": ["images/products/5/1.jpg", "images/products/5/2.jpg", "images/products/5/3.jpg", "images/products/5/4.jpg"],
+    "available": true,
+    "exclusive": true,
+    "rating": 4.3,
+    "reviews": 243
+  },
+  {
+    "id": "6",
+    "name": "MacBook Pro - M1 Max",
+    "description": "The Apple MacBook Pro is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 2199,
+    "image": "images/products/6/1.jpg",
+    "images": ["images/products/6/1.jpg", "images/products/6/2.jpg", "images/products/6/3.jpg", "images/products/6/4.jpg"],
+    "available": true,
+    "exclusive": false,
+    "rating": 4.5,
+    "reviews": 5430
+  },
+  {
+    "id": "7",
+    "name": "Apple Pro Display",
+    "description": "The Apple Pro Display is a premium all-in-one notebook computer with a stunningly crisp Retina display. It features an amazing thin and light design, and is powered by the latest Intel processors and the latest graphics. It also features a brilliant backlit keyboard and a full-size trackpad for a comfortable and responsive typing experience.",
+    "price": 4999,
+    "image": "images/products/7/1.jpg",
+    "images": ["images/products/7/1.jpg", "images/products/7/2.jpg", "images/products/7/3.jpg", "images/products/7/4.jpg"],
+    "available": true,
+    "exclusive": false,
+    "rating": 4.8,
+    "reviews": 1233
+  },
 ]
 
 export default productCatalog;
